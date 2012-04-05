c62e2dc0c177c9e75eec1fd57713509290681192
Tom Gallacher <tomgallacher23@gmail.com>
2012-04-05 19:05:08 +0100

    First working version

--- src/processor.js
+++ src/processor.js
@@ -3,71 +3,90 @@
  * @author Tom Gallacher
  */
 
- //Stop JS lint whining about jquery && BlobBuilder
-var $
-  , BlobBuilder
-  ;
-
 // imports the communication wrapper to work with webworkers and nodes cluster || child proccess fork
 // importScripts('disio-client-communication.js');
 // importScripts('workunit.js'); // data URI for the main work unit
 
 //time for some psuedo code
-$(function() {
-  var State = exports.createState()
-    , worker
+var socket = io.connect('http://localhost:8282')
+  , workunitId
+  ;
+
+socket.on('connect', function () {
+});
+
+socket.json.send({"action":"request"});
+
+socket.on('message', function(message) {
+  switch (message.action) {
+    // gets the workunit - the js to run a task
+    case 'workunit':
+      createWorkUnit(message);
+      socket.json.send({'action': 'getPayload'});
+      workunitId = message.workunitId;
+      break;
+    // gets the payload, this hold parameters
+    case 'payload':
+      message.workunitId = workunitId;
+      startWorkUnit(message);
+      break;
+    case 'message':
+      console.log(message);
+      break;
+    default:
+      throw new Error('Unknown Message action sent: ' + message.action);
+  }
+});
+
+var state = exports.createState()
+  , worker
+  ;
+
+function workerOnMessage(e) {
+  var data = e.data
     ;
 
-  function workerOnMessage(e) {
-    var data = JSON.parse(e.data)
-      ;
-    switch(data.type) {
-      case 'message':
-        break;
-      case 'saveState':
-          State.setState(data.workUnitID, data.state);
-        break;
-      case 'completion':
-          // change to WebSockets ;]
-          $.post('http://proxy.tomg.co', data, function(res) {
-            if (res.statusCode === 200) {
-              State.clearState();
-            } else {
-              worker.postMessage({
-                  'cmd' : 'start'
-                , 'state': false
-              });
-            }
-          });
-        break;
-    }
+  switch(data.action) {
+    case 'message':
+      console.log(data);
+      break;
+    case 'saveState':
+      state.setState(data.workUnitID, data.state);
+      break;
+    case 'completed':
+      data.workunitId = workunitId;
+      socket.json.send(data);
+      // state.clearState();
+      break;
   }
+}
 
-  function workerOnError(e) {
-    console.log('ERROR: Line ' + e.lineno + ' in ' + e.filename + ': ' + e.message);
-  }
+function workerOnError(e) {
+  // displays errors in the workers
+  // console.log('ERROR: Line ' + e.lineno + ' in ' + e.filename + ': ' + e.message);
+  console.log('Worker Error:');
+  console.log(e.stack);
+}
 
-  function getWorkUnit(cb) {
-    // create worker from remote source, this could be very ugly...
-    $.get('http://proxy.tomg.co', function(workUnit) {
-      var bb = new BlobBuilder()
-      ;
+function createWorkUnit(workUnit) {
+  // create worker from remote source, this could be very ugly...
+  // use socket.io
+  // var bb = new BlobBuilder()
+  var bb = new WebKitBlobBuilder()
+  ;
+  bb.append(workUnit.data);
+  // Note: window.webkitURL.createObjectURL() in Chrome 10+.
+  // worker = new Worker(window.URL.createObjectURL(bb.getBlob()));
+  worker = new Worker(window.webkitURL.createObjectURL(bb.getBlob()));
+  worker.addEventListener('message', workerOnMessage, false);
+  worker.addEventListener('error', workerOnError, false);
+}
 
-      bb.append(workUnit.data);
-      // Note: window.webkitURL.createObjectURL() in Chrome 10+.
-      worker = new Worker(window.URL.createObjectURL(bb.getBlob()));
-
-      worker.addEventListener('message', workerOnMessage, false);
-      worker.addEventListener('error', workerOnError, false);
-      cb(workUnit.id);
-    });
-  }
-
-  getWorkUnit(function(workUnitId) {
-    worker.postMessage({
-        'cmd' : 'start'
-      , 'workUnitId': workUnitId
-      , 'state': State.getState(workUnitId)
-    });
+function startWorkUnit(payload) {
+  worker.postMessage({
+      'cmd' : 'start'
+    , 'payload': payload.data.payload
+    , 'id': payload.data.id
+    , 'state': state.getState()
   });
-});
+}
