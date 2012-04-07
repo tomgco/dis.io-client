de784ac2a929dc167ddf02e0cc6abd3e0584fd9f
Tom Gallacher <tomgallacher23@gmail.com>
2012-04-08 00:12:42 +0100

    Cleaning of client code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express')
   , app = express.createServer()
-  , io = require('socket.io').listen(app);
+  , io = require('socket.io').listen(app)
+  , http = require('http')
+  ;
 
 app.configure(function(){
     app.use(express.static(__dirname));
@@ -11,6 +13,33 @@ app.listen(3001);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/test/index.html');
+});
+
+app.get('/distributors', function (req, res) {
+  var host = process.argv[2] || 'magellanic.local';
+  http.get({
+      host: host
+    , port: 3000
+    , path: '/distributors'
+  }, function(a) {
+    var data = "";
+    a.setEncoding('utf8');
+    a.on('data', function (chunk) {
+      data += chunk;
+    });
+
+    a.on('end', function() {
+      console.log(data);
+      if (a.statusCode === 200) {
+        res.json(JSON.parse(data));
+      } else {
+        res.end(500);
+      }
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+    res.end(500);
+  });
 });
 
 io.sockets.on('connection', function (socket) {
--- src/processor.js
+++ src/processor.js
@@ -8,85 +8,98 @@
 // importScripts('workunit.js'); // data URI for the main work unit
 
 //time for some psuedo code
-var socket = io.connect('http://localhost:8282')
-  , workunitId
-  ;
-
-socket.on('connect', function () {
+$.get('/distributors', function(data) {
+  var max = data.length - 1
+    , a = 0 + (Math.random() * ((max - 0) + 1))
+    , host = data[Math.floor(a)]
+    , uri = 'http://' + host.hosts[0] + ':' + host.port;
+  console.log(uri);
+  startProcess(uri);
 });
 
-socket.json.send({"action":"request"});
-
-socket.on('message', function(message) {
-  switch (message.action) {
-    // gets the workunit - the js to run a task
-    case 'workunit':
-      createWorkUnit(message);
-      socket.json.send({'action': 'getPayload'});
-      workunitId = message.workunitId;
-      break;
-    // gets the payload, this hold parameters
-    case 'payload':
-      message.workunitId = workunitId;
-      startWorkUnit(message);
-      break;
-    case 'message':
-      console.log(message);
-      break;
-    default:
-      throw new Error('Unknown Message action sent: ' + message.action);
-  }
-});
-
-var state = exports.createState()
-  , worker
-  ;
-
-function workerOnMessage(e) {
-  var data = e.data
+function startProcess(uri) {
+  var socket = io.connect(uri)
+    , workunitId
     ;
 
-  switch(data.action) {
-    case 'message':
-      console.log(data);
-      break;
-    case 'saveState':
-      state.setState(data.workUnitID, data.state);
-      break;
-    case 'completed':
-      data.workunitId = workunitId;
-      socket.json.send(data);
-      // state.clearState();
-      break;
-  }
-}
-
-function workerOnError(e) {
-  // displays errors in the workers
-  // console.log('ERROR: Line ' + e.lineno + ' in ' + e.filename + ': ' + e.message);
-  console.log('Worker Error:');
-  console.log(e.stack);
-}
-
-function createWorkUnit(workUnit) {
-  // create worker from remote source, this could be very ugly...
-  // use socket.io
-  // var bb = new BlobBuilder()
-  var bb = new WebKitBlobBuilder()
-  ;
-  bb.append(workUnit.data);
-  // Note: window.webkitURL.createObjectURL() in Chrome 10+.
-  // worker = new Worker(window.URL.createObjectURL(bb.getBlob()));
-  worker = new Worker(window.webkitURL.createObjectURL(bb.getBlob()));
-  worker.addEventListener('message', workerOnMessage, false);
-  worker.addEventListener('error', workerOnError, false);
-}
-
-function startWorkUnit(payload) {
-  worker.postMessage({
-      'cmd' : 'start'
-    , 'payload': payload.data.payload
-    , 'id': payload.data.id
-    , 'state': state.getState()
+  socket.on('connect', function () {
   });
+
+  socket.json.send({"action":"request"});
+
+  socket.on('message', function(message) {
+    switch (message.action) {
+      // gets the workunit - the js to run a task
+      case 'workunit':
+        createWorkUnit(message);
+        socket.json.send({'action': 'getPayload'});
+        workunitId = message.workunitId;
+        break;
+      // gets the payload, this hold parameters
+      case 'payload':
+        message.workunitId = workunitId;
+        setTimeout(function() {
+          startWorkUnit(message);
+        }, 1500);
+        break;
+      case 'message':
+        console.log(message);
+        break;
+      default:
+        throw new Error('Unknown Message action sent: ' + message.action);
+    }
+  });
+
+  var state = exports.createState()
+    , worker
+    ;
+
+  function workerOnMessage(e) {
+    var data = e.data
+      ;
+
+    switch(data.action) {
+      case 'message':
+        console.log(data);
+        break;
+      case 'saveState':
+        state.setState(data.workUnitID, data.state);
+        break;
+      case 'completed':
+        data.workunitId = workunitId;
+        socket.json.send(data);
+        // state.clearState();
+        break;
+    }
+  }
+
+  function workerOnError(e) {
+    // displays errors in the workers
+    // console.log('ERROR: Line ' + e.lineno + ' in ' + e.filename + ': ' + e.message);
+    console.log('Worker Error:');
+    console.log(e.stack);
+  }
+
+  function createWorkUnit(workUnit) {
+    // create worker from remote source, this could be very ugly...
+    // use socket.io
+    // var bb = new BlobBuilder()
+    var bb = new WebKitBlobBuilder()
+    ;
+    bb.append(workUnit.data);
+    // Note: window.webkitURL.createObjectURL() in Chrome 10+.
+    // worker = new Worker(window.URL.createObjectURL(bb.getBlob()));
+    worker = new Worker(window.webkitURL.createObjectURL(bb.getBlob()));
+    worker.addEventListener('message', workerOnMessage, false);
+    worker.addEventListener('error', workerOnError, false);
+  }
+
+  function startWorkUnit(payload) {
+    worker.postMessage({
+        'cmd' : 'start'
+      , 'payload': payload.data.payload
+      , 'id': payload.data.id
+      , 'state': state.getState()
+    });
+  }
 }
